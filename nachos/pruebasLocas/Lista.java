08301dc23756b3a98893641df8c35f4f3362db35
¨Eduardo <eduardo_2901@hotmail.com>
2011-02-27 15:31:17 -0600

    Primer Commit

---
+++ nachos/pruebasLocas/Lista.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Lista{
+	
+	
+		public static void main(String args[]) throws Exception{
+			
+		LinkedList<Integer> listita = new LinkedList<Integer>();	
+		
+			for (int i = 0; i < 0; i++){
+				listita.add(i);
+			}
+			
+			System.out.println(listita.getFirst());
+		}
+	
+	
+}
